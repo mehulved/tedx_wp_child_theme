9b636d1c74fbd422c5236d4d9067d396c7ff36fa
Mehul Ved <mehul@mehulved.com>
2012-10-17 17:06:27 +0530

    new font added for headings

--- functions.php
+++ functions.php
@@ -100,6 +100,7 @@ add_action('admin_menu', 'tedx_theme_admin_menu');
 
 function tedx_scripts() {
     wp_enqueue_script('tedx_frontend_script', get_stylesheet_directory_uri() .'/assets/js/tedx_theme-scripts.js', array('jquery'));
+    wp_enqueue_style('tedx_heading_font', get_stylesheet_directory_uri() .'/assets/fonts/museoslab/stylesheet.css');
 }
 add_action('wp_enqueue_scripts', 'tedx_scripts');
 
