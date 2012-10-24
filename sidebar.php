99b457ac14a0675f4d1303bd4993dd4b8c17049d
Mehul Ved <mehul@mehulved.com>
2012-10-24 22:17:19 +0530

    updated social links in the sidebar to open in a new window

--- sidebar.php
+++ sidebar.php
@@ -9,16 +9,16 @@
                             <li id="sm_sidebar">
                                 <h2>Follow <?php echo get_bloginfo('name'); ?></h2>
                                 <div class="textwidget">
-                                    <a href="<?php echo get_bloginfo('rss2_url');?>" rel="alternate" type="application/rss+xml">
+                                    <a href="<?php echo get_bloginfo('rss2_url');?>" rel="alternate" type="application/rss+xml" target="_blank">
                                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/rss.png" alt="Subscribe to RSS Feed" />
                                     </a>
-                                    <a href="<?php echo get_option('tedx_twitter_url'); ?>" rel="nofollow">
+                                    <a href="<?php echo get_option('tedx_twitter_url'); ?>" rel="nofollow" target="_blank">
                                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter.png" alt="Follow on Twitter" />
                                     </a>
-                                    <a href="<?php echo get_option('tedx_fb_url'); ?>" rel="nofollow">
+                                    <a href="<?php echo get_option('tedx_fb_url'); ?>" rel="nofollow" target="_blank">
                                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/facebook.png" alt="Like on Facebook" />
                                     </a>
-                                    <a href="<?php echo get_option('tedx_yt_url'); ?>" rel="nofollow">
+                                    <a href="<?php echo get_option('tedx_yt_url'); ?>" rel="nofollow" target="_blank">
                                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/youtube.png" alt="View Videos on Youtube" />
                                     </a>
                             </li>
